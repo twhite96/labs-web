6897db1d2cb23dd83a9f464df57b299ea382fa0f
Tiffany <tiffany@tiffanywhite.dev>
2023-08-23 00:28:37 -0400

    Update something

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230823.002434';
+const cacheName = 'chirpy-20230823.002729';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
