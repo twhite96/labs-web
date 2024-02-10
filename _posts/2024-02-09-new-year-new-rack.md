6aa8202467fbb7e208c15ac1e4099f32b5563fba
tiff <tiffany@tiffanywhite.dev>
2024-02-10 01:06:09 -0500

    Update something

--- _posts/2024-02-09-new-year-new-rack.md
+++
@@ -1,8 +0,0 @@
----
-title: New Year, New Rack
-excerpt: Upgrading the runs and organization in the rack
-tags: [meta homelab]
-category: [Homelab]
-pin: true
-draft: true
----
--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240210.005847';
+const cacheName = 'chirpy-20240210.010543';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
