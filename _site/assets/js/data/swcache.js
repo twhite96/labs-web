fa45cf8b927aab3d80b9aae3d947618dba0dda36
tiff <github@0x8c.org>
2024-08-21 06:02:52 -0400

    Update something

--- _site/assets/js/data/swcache.js
+++ _site/assets/js/data/swcache.js
@@ -35,7 +35,7 @@ const resource = [
 const allowedDomains = [
     
 
-    'localhost:4004',
+    'tifflabs.org',
 
     
         'res.cloudinary.com',
--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240629.210504';
+const cacheName = 'chirpy-20240821.060232';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
