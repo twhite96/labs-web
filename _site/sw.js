b56793a64b15c992de7743a24a4b357a129adcde
tiff <twhite96@users.noreply.github.com>
2025-02-22 17:17:04 -0500

    Update something

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20250220.235806';
+const cacheName = 'chirpy-20250222.171647';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
